1d82d38fcf017d8e97f568c7d4425188336a80fd
Narcis Radoi <40882676+narcisradoi@users.noreply.github.com>
2019-10-20 21:15:22 +0100

    added some interface v1

--- seotoolsv1/urls.py
+++ seotoolsv1/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from kdchecker import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('kwdc/', include('kdchecker.urls')),
+    path('', include('kdchecker.urls')),
+    path('process/', views.process, name="process"),
 ]
